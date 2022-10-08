f1d256629c0633f94f26aab7953a6a76d58e7211
WARFADE <96909243+WARFADE-BOSS@users.noreply.github.com>
2022-10-09 03:09:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Montserrat:wght@400;600&display=swap" rel="stylesheet"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  <title>NOJOTO BY WARFADE</title></head><style>
-*{ margin: 0; padding: 0; box-sizing: border-box; font-family: 'Bree Serif', serif; } body{ width: 100%; height: 100vh; text-align: center; background-color: rgb(255, 255, 255); /* overflow: hidden; */ } .limiter{ width: 100%; padding: 0 25px; } .limiter h1{ font-size: 20px; font-family: 'Bree Serif', serif; font-weight: 300; text-transform: uppercase; letter-spacing: 1px; margin-top: 50px; margin-bottom: 50px; }
-input{ font-family: 'Bree Serif', serif; width: 100%; height: 45px; font-weight: 200;
+*{ 
+margin: 0; 
+padding: 0; 
+box-sizing: border-box; 
+font-family: 'Bree Serif', serif; 
+} 
+
+body{ 
+width: 100%; 
+height: 100vh; 
+text-align: center; 
+background-color: rgb(255, 255, 255); /* overflow: hidden; */ 
+} 
+
+.limiter{ 
+width: 100%; 
+padding: 0 25px; 
+} 
+
+.limiter h1{ 
+font-size: 20px; 
+font-family: 'Bree Serif', serif; 
+font-weight: 300; 
+text-transform: uppercase;
+letter-spacing: 1px; 
+margin-top: 50px; 
+margin-bottom: 50px; 
+}
+
+input,button{ font-family: 'Bree Serif', serif; width: 100%; height: 45px; font-weight: 200;
 padding: 12px 32px;
   text-align: center;
   text-decoration: none;
@@ -13,9 +41,20 @@ border: 2px solid rgb(0, 141, 146);
   }
 .input{ border: 1.5px solid rgb(211, 211, 211); background-color: transparent; padding: 0 15px; transition: 0.5s; }
  .input:focus{ border: 1.5px solid rgb(0, 141, 146); }
-  .submit{ margin: 15px 0; border: none; background-color: rgb(0, 141, 146); color: #fff; text-transform: uppercase; letter-spacing: 2px; box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.349); transition: 0.5s; } .submit:hover{ background-color: rgb(7, 89, 94); } .telegram{ font-family: 'Bree Serif', serif; width: 100%; height: 65px; font-weight: 200; margin: 30px 0; border-radius: 4px; background-color: rgb(0, 141, 146); display: flex; justify-content: center; align-items: center; box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.349); } .telegram i{ color: #fff; font-size: 30px; margin-left: -30px; } .telegram a{ color: #fff; text-decoration: none; font-weight: 300; letter-spacing: 2px; margin-left: 10px; font-size: 16px; text-transform: uppercase; } .success{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #d1e6dd; color: #105133; border: 1px solid #9fcfba; } .error{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #f7d7da; color: #84200a; border: 1px solid #f5c2c7; } .white{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #e2e3e5; color: #41464a; border: 1px solid #d5dbdb; } .blue{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #cfe2ff; color: #084195; border: 1px solid #d7d4fe; }
+  .submit, button{ margin: 15px 0; border: 2px solid black; background-color: rgb(0, 141, 146); color: #fff; text-transform: uppercase; letter-spacing: 2px; box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.349); transition: 0.5s; } .submit:hover{ background-color: rgb(7, 89, 94); } .telegram{ font-family: 'Bree Serif', serif; width: 100%; height: 65px; font-weight: 200; margin: 30px 0; border-radius: 4px; background-color: rgb(0, 141, 146); display: flex; justify-content: center; align-items: center; box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.349); } .telegram i{ color: #fff; font-size: 30px; margin-left: -30px; } .telegram a{ color: #fff; text-decoration: none; font-weight: 300; letter-spacing: 2px; margin-left: 10px; font-size: 16px; text-transform: uppercase; } .success{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #d1e6dd; color: #105133; border: 1px solid #9fcfba; } .error{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #f7d7da; color: #84200a; border: 1px solid #f5c2c7; } .white{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #e2e3e5; color: #41464a; border: 1px solid #d5dbdb; } .blue{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #cfe2ff; color: #084195; border: 1px solid #d7d4fe; }
 
- select { width: 100%; height: 65px; margin: 15px 0; border-radius: 4px;		padding:0 20px;		font-size: 15px;		outline: none; font-family: 'Bree Serif', serif; }</style><body> <div class="limiter">
+ select { 
+width: 100%; 
+height: 65px; 
+margin: 15px 0; 
+border-radius: 4px;		
+padding:0 20px;		
+font-size: 15px;		
+outline: none; 
+font-family: 'Bree Serif', serif; 
+}
+</style>
+<body> <div class="limiter">
    <h1 class="success">NOJOTO</h1>
    <?php
 error_reporting(0);
@@ -116,6 +155,8 @@ if($j=="Reward Redeemed Successfully"){
 		  
 	  ';
 }
-
 ?>
+<br>
+	<button onClick="window.location='https://tttttt.me/KhasGiveways'">JOIN TELEGRAM</button>
+
 <div class="success">By ==> WarFade</div>
