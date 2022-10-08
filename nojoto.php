52197b1c2ee9df28c70208055c4c05ea0178e933
WARFADE <96909243+WARFADE-BOSS@users.noreply.github.com>
2022-10-08 23:18:36 +0530

    Add files via upload

---
+++ nojoto.php
@@ -0,0 +1,170 @@
+<html>
+<head>
+
+<title>S.R.SCRIPT</title> 
+<meta name="viewport" content="width=device-width">
+<link href="/favicon.ico" rel="icon" type="image/x-icon" /> <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre|Laila" rel="stylesheet">
+<style>
+a{font-weight:bold;text-decoration:none;}
+body,input{text-align:center;letter-spacing:1px;font-family: arial;}
+hr{border: 0;height: 0.7px;background-image:linear-gradient(to right, red, blue, Green);}input,select{outline:none;}
+.text,select{background-image:linear-gradient(to right, green, green);width: 75%;padding: 11px 20px;margin: 8px 1;display: inline-block; border: 1px solid ;border-radius: px; box-sizing: border-box;text-align:center;font-weight:bold;color:white;
+}
+::placeholder{font-weight:bold;color:black;}
+.submit{font-weight:bold;width: 45%;background-image:linear-gradient(to right, green, blue);color: white;padding: 14px 20px;margin: 8px 0;border: none;border-radius: 4px;cursor: pointer;}
+.submit:hover{background-image:linear-gradient(to right, blue);}
+
+bold{color:linear-gradient(to right, red, blue, red);font-weight:bold;}body{color:#F50157
+;}
+
+</style>
+<script>if(top!=self){ top.location.replace(document.location); alert("[S.R. SCRIPT]For Security Reasons, Framing is Not Allowed, Click OK to Remove The Frames")}</script><html><head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes"><meta charset="UTF-8"><style>body{background:	white}</style><style>#rcorners1 {border-radius: 2px;background: #73AD21;padding: 20px; width: 200px;height: 50px; }#rcorners2 {border-radius: 25px;border: 2px solid #73AD21;padding: 20px; width: 280px;height: 50px; font-size: 15px;}#rcorners2 h3 {padding: 0px;margin: 5px;font-size: 20px;}#rcorners3 {border-radius: 50%;background: #22bbcc;background-repeat: repeat;padding: 10%; text-align: center;width: 200px;height: 180px; }#rcorners3 img{display: block;margin: 0 0 0 -20px;width: 250px;}
+input[type=submit], button { width: 40%; background-color: #4CAF50; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer;}
+input[type=tel], select {width: 75%;padding: 12px 20px;margin: 8px 0;display: inline-block;border: 1px solid #ccc;border-radius: 15px;box-sizing: border-box;}input[type=submit] {width: 40%;background-color: #45a049;color: white;padding: 14px 20px;margin: 8px 0;border: none;border-radius: 15px;cursor: pointer;}input[type=submit]:hover {background-color: #45a049;}div {border-radius: 5px;background-color: #f2f2f2;padding: 20px;}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 13px;
+  font-weight: 400;
+  background: #0B2647;
+  color: Red;
+  line-height: 1.5;
+  letter-spacing: 0.10px;
+  text-align: center;
+  height: 100vh;
+}
+
+
+
+
+summary {
+  font-size: 0.83rem;
+  font-weight: 600;
+  background-color: #fff;
+  color: #333;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  outline: none;
+  border-radius: 0.25rem;
+  text-align: left;
+  cursor: pointer;
+  position: relative;
+}
+
+</style></head><title>S.R.SCRIPT</title><body><center><b><font color=blue><b><hr>Nojoto Refer Script<br><font style="color: green; font-size:15px">
+</font></font></b><center>
+   <?php
+error_reporting(0);
+$url="https://t.me/SRSCRIPT1";
+$refer=$_GET['refer'];
+$le=file_get_contents("rell.txt");
+if($refer){
+	$file="ref/$refer.txt";
+
+	$s=file_get_contents($file);
+	$s1=$s+1;
+	if($s>=30){
+		echo'<h1 class="error">Your Refer Limit Over</h1>';
+		heeader("Refresh:0.1 url=$url");
+	}else{
+	
+	
+function Rando($length) {
+    $characters = '0123456789abcdefghijklmno';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+function httpCall($url, $data = "", $headers = array(), $method = "GET", $headerReturn = 0) {
+  if(empty($headers)){
+  	$ip = long2ip(rand());
+    $headers=["User-Agent: okhttp/3.12.1","Accept-Encoding: gzip, deflate","X-Forwarded-For: $ip"];
+  }
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_HEADER, $headerReturn);
+  curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+  $output = curl_exec($ch);
+  return $output;
+}
+$f = array("Vasu","Nirmal","Akshay","Chander","Rupinder","Akhil","Shanti","Ravi","Kunal","Chandrakant","Sulabha","Mahinder","Swapnil","Deepa","Sulabha","Neelima","Vijaya","Nikhil","Isha","Siddhi","Ajeet","Kshitija","Anila","Jitender","Sumeet","Preethi","Priti","Gayathri","Dhaval","Mukesh","Lalita","Rachana","Rakhi","Harshal","Shekhar","Rajiv","Balakrishna","Ajeet","Tara","Chander","Deepa","Prabhu","Rajendra","Jeetendra","Nandu","Aniket","Sumati","Prabhu","Vimal","Indira","Laxman","Agni","Kapil","Kailash","Puneet","Pratik","Pankaj","Ishore","Swati","Rupa","Hardeep","Prabhu","Khushi","Gurmeet","Nishant","Rishi","Naveen");
+$fname = $f[mt_rand(0,60)];
+ $l = array("sameer","suresh","chettiar","jatin","chauhan","agarwal","rahul","tanmay","tiwari","kunal","rasania","sunil","kumar","gaurav","arihant","jain","falguni","yashashree","arpi","arshish","gupta","tanmay","samtgr","kiyera","atul","abhay","chandra","shoibaakriti","aanchal","talreja","aatholiy","abhijeet","akkalwar","abhijeet","bajpai","abhijeetsh","abhirup","roy","abhishek","sumit","kapil","rani","ramu","souvik","yogesh","umesh","sahadat","ankit","prasant","pravakar","sunil","sibaram");
+$lname = $l[mt_rand(0,50)];
+$a1=mt_rand(63,99);
+$a2=mt_rand(11111111,88888888);
+$number="$a1$a2";
+$de=Rando(16);
+$app_version="2.0.58";
+$hash=md5("|njt|-$de-|njt|-$de-|njt|-$app_version-|njt|");
+$output=httpCall("https://nojoto.com/api/v1/auth.php?njtDeviceId=$de&njtDeviceName=$de&njtAppVersion=2.0.58&njtSecurityKey=$hash&xtoken=&languageId=");
+$xtoken=json_decode($output,1)['result']['xtoken'];
+$ip = long2ip(rand());
+$headers=["User-Agent: okhttp/3.12.1","Accept-Encoding: gzip, deflate","content-type: application/x-www-form-urlencoded","X-Forwarded-For: $ip"];
+$url="https://nojoto.com/api/v1/auth.php";
+
+
+$data="socialLongLiveAccessToken=EAAaLL7L7Tm0BAIA8Nh1oN5eln0voPadnYZClEWnvEYjIzODZBFjEZCmzW18HB7ZBEN9YSW2F7tM4u8yQYfGqzSweuJiU5wJLYzIYxbOV7zexNZA1hB2YHqkM62oSnVYfru9Pviz9GHYutg8PU3fYZBdZA1Bu5LEa6vm2izZBi636jicCZC1FNhsIr6BZAk7EgzdupVoIH14diP2skLNOteYdix95iGtyk4w9A24rZA9WInpyjKgeCz8ZAq7z&gender=&isSocialEmail=yes&socialUniqueId=14039630".mt_rand(1111111,9999999)."&languageId=1&photo=https%3A%2F%2Fplatform-lookaside.fbsbx.com%2Fplatform%2Fprofilepic%2F%3Fasid%3D140396302038756%26height%3D50%26width%3D50%26ext%3D1667536817%26hash%3DAeQ0ImTiHI8otGOdwcc&type=FB&isMultiAccountLogin=1&socialAccessToken=EAAaLL7L7Tm0BAIA8Nh1oN5eln0voPadnYZClEWnvEYjIzODZBFjEZCmzW18HB7ZBEN9YSW2F7tM4u8yQYfGqzSweuJiU5wJLYzIYxbOV7zexNZA1hB2YHqkM62oSnVYfru9Pviz9GHYutg8PU3fYZBdZA1Bu5LEa6vm2izZBi636jicCZC1FNhsIr6BZAk7EgzdupVoIH14diP2skLNOteYdix95iGtyk4w9A24rZA9WInpyjKgeCz8ZAq7z&dob=&v=1&name=$fname$lname&location=&xtoken=$xtoken&email=$fname$lname$number%40gmail.com";
+
+$output=httpCall($url,$data,$headers,"POST",0);
+$headers=["User-Agent: okhttp/3.12.1","Accept-Encoding: gzip, deflate","content-type: application/x-www-form-urlencoded","X-Forwarded-For: $ip"];
+$url="https://nojoto.com/api/v1/auth.php";
+$data="socialLongLiveAccessToken=EAAaLL7L7Tm0BAIA8Nh1oN5eln0voPadnYZClEWnvEYjIzODZBFjEZCmzW18HB7ZBEN9YSW2F7tM4u8yQYfGqzSweuJiU5wJLYzIYxbOV7zexNZA1hB2YHqkM62oSnVYfru9Pviz9GHYutg8PU3fYZBdZA1Bu5LEa6vm2izZBi636jicCZC1FNhsIr6BZAk7EgzdupVoIH14diP2skLNOteYdix95iGtyk4w9A24rZA9WInpyjKgeCz8ZAq7z&gender=&isSocialEmail=yes&socialUniqueId=14039630".mt_rand(1111111,9999999)."&languageId=1&photo=https%3A%2F%2Fplatform-lookaside.fbsbx.com%2Fplatform%2Fprofilepic%2F%3Fasid%3D140396302038756%26height%3D50%26width%3D50%26ext%3D1667536817%26hash%3DAeQ0ImTiHI8otGOdwcc&type=FB&isMultiAccountLogin=1&socialAccessToken=EAAaLL7L7Tm0BAIA8Nh1oN5eln0voPadnYZClEWnvEYjIzODZBFjEZCmzW18HB7ZBEN9YSW2F7tM4u8yQYfGqzSweuJiU5wJLYzIYxbOV7zexNZA1hB2YHqkM62oSnVYfru9Pviz9GHYutg8PU3fYZBdZA1Bu5LEa6vm2izZBi636jicCZC1FNhsIr6BZAk7EgzdupVoIH14diP2skLNOteYdix95iGtyk4w9A24rZA9WInpyjKgeCz8ZAq7z&dob=&v=1&name=$fname%20$lname&location=&xtoken=$xtoken&email=$fname$lname$number%40gmail.com";
+
+$output=httpCall($url,$data,$headers,"POST",0);
+$j=json_decode($output,1);
+$cid=$j['cid'];
+if($cid){
+
+$output=httpCall("https://nojoto.com/api/v1/referral.php?cid=$cid&xtoken=$xtoken&rewardCode=$refer&isManualScratch=1",'','',"GET",0);
+$j=json_decode($output,1)["message"];
+if($j=="Reward Redeemed Successfully"){
+	echo "<div class=''><center>
+<font color='green' size='3'><b>Refer Done Successfully!</b></font></div></center><hr>";
+$ma=file_get_contents("rell.txt");
+$r=$ma+1;
+$mjj=file_put_contents("rell.txt","$r");
+	echo"<hr class='style5'></font></center><meta http-equiv='refresh' content=1;url=https://t.me/SRSCRIPT1>";
+	mkdir("ab");
+	file_put_contents($file,$s1);
+	header("Refresh:5.1");
+}else{
+	echo "<div class=''><center>
+<font color='red' size='3'><b>Refer Failed</b></font></div></center><hr>";
+	header("Refresh:5.1");
+echo"<hr class='style5'></font></center><meta http-equiv='refresh' content=1;url=https://t.me/SRSCRIPT1>";
+}
+}else{
+	echo "<div class=''><center>
+<font color='red' size='3'><b>Refer Failed</b></font></div></center><hr>";
+	header("Refresh:5.1");
+	echo"<hr class='style5'></font></center><meta http-equiv='refresh' content=1;url=https://t.me/SRSCRIPT1>";
+}
+
+}
+}else{
+	echo '	
+	<form action="" method="GET">
+	<input type="tel" name="refer" placeholder="Enter Refer Code" required>
+	
+	<input type="submit" class="submit">
+		  
+		  </form>
+		  
+	  ';
+echo "<div class=''><center>
+<font color='red' size='3'><b>Total Success Refer- $le</b></font></div></center><hr>";
+}
+
+?>
+<hr><center><font color='blue'><marquee><center><bold><center><font color='blue' size='3px'><center>Script Made By Sahajul<br>
